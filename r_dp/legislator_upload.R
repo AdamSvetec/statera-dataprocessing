261044718eef1d319b97cdf63c09f23cb30e205c
Adam Svetec <adamsvetec@gmail.com>
2016-11-25 17:58:51 +0000

    Add default file location so no arguments needed Fix problems with reading csv and add read of bioguide_id needed for voting records Remove need for database password, stored locally

--- r_dp/legislator_upload.R
+++ r_dp/legislator_upload.R
@@ -2,35 +2,16 @@
 
 #Parse command line arguments to get filepath of legislators-historic.csv
 args = commandArgs(trailingOnly=TRUE)
-if(length(args) != 2){
-  stop("Filename of legislator csv and database password must be provided")
+filename = '~/BulkData/legislators-historic.csv'
+if(length(args) > 0){
+  filename = args[1]
 }
-filename = args[1]
-db.password = args[2]
 
 #Read in legislators table from file
-legislators = read.table(filename, header = TRUE, sep = ",", fill=TRUE)
+legislators = read.table(filename, header = TRUE, sep = ",", fill=TRUE, stringsAsFactors = FALSE)
 
 #Remove all unused columns
-legislators$url <- NULL
-legislators$address <- NULL
-legislators$phone <- NULL
-legislators$contact_form <- NULL
-legislators$rss_url <- NULL
-legislators$twitter <- NULL
-legislators$facebook <- NULL
-legislators$facebook_id <- NULL
-legislators$youtube <- NULL
-legislators$youtube_id <- NULL
-legislators$bioguide_id <- NULL
-legislators$thomas_id <- NULL
-legislators$lis_id <- NULL
-legislators$cspan_id <- NULL
-legislators$votesmart_id <- NULL
-legislators$ballotpedia_id <- NULL
-legislators$washington_post_id <- NULL
-legislators$icpsr_id <- NULL
-legislators$wikipedia_id <- NULL
+legislators = subset(legislators, select = c(last_name, first_name, birthday, gender, type, state, district, party, govtrack_id, bioguide_id))
 
 #Convert birthday strings to dates and remove all legislators born earlier than 1900
 legislators$birthday <- as.Date(legislators$birthday)
@@ -40,11 +21,10 @@ legislators <- legislators[years > 1900,]
 
 #tail(legislators, 5)
 
-#Open MariaDB connection
 library(RMySQL)
 
 #Testing functionality on 'test' database. works, now need to implement for actual dataprocessing database 
-#con <- dbConnect(RMySQL::MySQL(), dbname = "test", user = "root", password = db.password)
-#dbWriteTable(con, "politician", legislators, row.names = FALSE, overwrite = TRUE)
-#dbDisconnect(con)
+con <- dbConnect(RMySQL::MySQL(), group="data-processing")
+dbWriteTable(conn=con, name="legislator", value=legislators, row.names = FALSE, overwrite = TRUE)
+dbDisconnect(con)
 
