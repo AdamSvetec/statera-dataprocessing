85cee60a22f027e38014ad3525f34cabfb8f6850
Adam Svetec <adamsvetec@gmail.com>
2016-11-26 20:32:45 +0000

    Fix to govTrack upload, opensecrets_id column was not read correctly

--- r_dp/legislator_upload.R
+++ r_dp/legislator_upload.R
@@ -1,30 +1,34 @@
-#Data upload for legislator data
+#Data upload for legislator data from govTrack
+#Standard Environment cleansing
+rm(list=ls())
+#Will use file specified in command line arguments or will use default:
+filename = '~/BulkData/legislators-historic.csv'
 
 #Parse command line arguments to get filepath of legislators-historic.csv
 args = commandArgs(trailingOnly=TRUE)
-filename = '~/BulkData/legislators-historic.csv'
 if(length(args) > 0){
   filename = args[1]
 }
 
 #Read in legislators table from file
-legislators = read.table(filename, header = TRUE, sep = ",", fill=TRUE, stringsAsFactors = FALSE)
+legislators = read.table(filename, header=TRUE, sep=",", quote="\"", fill=TRUE, stringsAsFactors=FALSE)
 
 #Remove all unused columns
-legislators = subset(legislators, select = c(last_name, first_name, birthday, gender, type, state, district, party, govtrack_id, bioguide_id))
+legislators = subset(legislators, select = c(last_name, first_name, birthday, gender, type, state, district, party, bioguide_id, opensecrets_id))
 
 #Convert birthday strings to dates and remove all legislators born earlier than 1900
-legislators$birthday <- as.Date(legislators$birthday)
-legislators <- legislators[!is.na(legislators$birthday),]
-years <- as.numeric(format(legislators$birthday, "%Y"))
-legislators <- legislators[years > 1900,]
+legislators <- legislators[legislators$opensecrets_id != "",]
+#legislators$birthday <- as.Date(legislators$birthday)
+#legislators <- legislators[!is.na(legislators$birthday),]
+#years <- as.numeric(format(legislators$birthday, "%Y"))
+#legislators <- legislators[years > 1900,]
 
 #tail(legislators, 5)
 
 library(RMySQL)
 
-#Testing functionality on 'test' database. works, now need to implement for actual dataprocessing database 
+#Upload legislators table to database
 con <- dbConnect(RMySQL::MySQL(), group="data-processing")
-dbWriteTable(conn=con, name="legislator", value=legislators, row.names = FALSE, overwrite = TRUE)
+dbWriteTable(conn=con, name="legislator_govtrack", value=legislators, row.names = FALSE, overwrite = TRUE)
 dbDisconnect(con)
 
