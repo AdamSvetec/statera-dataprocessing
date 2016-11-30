237056a4a7c106d2bd758941933f2219fb1a3a0f
Adam Svetec <adamsvetec@gmail.com>
2016-11-30 04:46:08 +0000

    Add script to migrate result tables to web application server

---
+++ scripts/results_table_transfer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+local_db="dataprocessing"
+web_server_url="ec2-54-212-210-64.us-west-2.compute.amazonaws.com"
+web_server_ip="172.31.34.188"
+remote_db="webserver"
+table_names="final_org_results issue organization organization_score"
+
+read -s -p "Enter password for local database: " local_pass
+echo ""
+read -s -p "Enter password for remote database: " remote_pass
+echo ""
+
+mysqldump --password=$local_pass $local_db $table_names | ssh -i ~/MyKey.pem ubuntu@$web_server_url sudo mysql --password=$remote_pass $remote_db
