bae4b40c79cf2046edd13558163fc29bcce85b1f
Adam Svetec <adamsvetec@gmail.com>
2016-12-08 18:36:31 +0000

    remove unused database init

--- scripts/database_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Initializing tables in database
-
-read -s -p "Please enter the password for database admin: " pass
-
-sudo mysql --password=$pass dataprocessing < ../db_utils/TableDeclarations.sql
-
-#sudo mysql --password=$pass test < testingscript.sql
